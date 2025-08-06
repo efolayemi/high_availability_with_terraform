ff229805ae5293c9083b48f0893707efe8754e2c
serfolaanu <folayemiemoruwa@gmail.com>
2025-08-06 05:46:40 +0100

    created a userdata.sh for EC2 instance configuration

--- userdata.sh
+++ userdata.sh
@@ -8,4 +8,4 @@ systemctl enable httpd
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html 
 echo "Healthy" > /var/www/html/health.html
 sudo yum install mysql -y
-mysql -h ${aws_db_instance.default[0].address} -P 3306 -u ${var.db_username} -p${var.db_password} -e 'show databases;'
+mysql -h "${aws_db_instance.default[0].id}" -P 3306 -u "${var.db_username}" -p"${var.db_password}" -e 'show databases;'
